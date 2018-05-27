94ed1940b45d408735308ce1f88a063ff9bad14b
zhangxiaohong123 <zxhces@126.com>
2018-05-27 20:28:19 +0800

    严重优先级

--- scripts/test.py
+++ scripts/test.py
@@ -7,6 +7,7 @@ class Test_001:
         allure.attach('描述', '我是测试步骤002的描述~~~')
         assert a!=2
 
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     @allure.step(title="测试步骤002")
     @pytest.mark.parametrize("b", [5, 4, 6])
     def test_002(self, b):
