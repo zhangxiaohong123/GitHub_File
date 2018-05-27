f8a23f43350026d602b54a8e956dee16025eed3c
zhangxiaohong123 <zxhces@126.com>
2018-05-27 17:28:19 +0800

    增加步骤描述

--- scripts/test.py
+++ scripts/test.py
@@ -4,9 +4,11 @@ class Test_001:
     @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a",[1,2,3,4,6])
     def test_001(self,a):
+        allure.attach('描述', '我是测试步骤002的描述~~~')
         assert a!=2
 
     @allure.step(title="测试步骤002")
     @pytest.mark.parametrize("b", [5, 4, 6])
     def test_002(self, b):
+        allure.attach('描述','我是测试步骤002的描述~~~')
         assert b != 2
