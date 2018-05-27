9d66f191187059eb11b0c99a8e92f3bfd876fb56
zhangxiaohong123 <zxhces@126.com>
2018-05-27 16:18:40 +0800

    增加测试步骤

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,12 @@
-import pytest
+import pytest,allure
 
 class Test_001:
+    @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a",[1,2,3,4,6])
-    def test_abc(self,a):
+    def test_001(self,a):
         assert a!=2
+
+    @allure.step(title="测试步骤002")
+    @pytest.mark.parametrize("b", [5, 4, 6])
+    def test_002(self, b):
+        assert b != 2
